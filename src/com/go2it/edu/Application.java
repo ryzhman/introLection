640c59cf0208d19d226878975795221ab8b035fb
Oleksandr Ryzhkov <oryzhkov@paymentus.com>
2019-05-06 23:37:25 -0400

    Initial setup: main() method and gitignore

---
+++ src/com/go2it/edu/Application.java
@@ -0,0 +1,13 @@
+package com.go2it.edu;
+
+/**
+ * In this class we will create do all the coding
+ *
+ * @author Alex Ryzhkov
+ */
+public class Application {
+
+	public static void main(String[] args) {
+		System.out.println("Woohoo! This is my first program!");
+	}
+}
